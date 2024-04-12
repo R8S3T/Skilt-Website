312994917060551ab76bc95ed6927648ab9249a7
Rebecca <r.stelter@gmx.com>
2024-04-12 22:43:31 +0200

    Add new demo image, change styling from about section, continue styling features section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import '../styles/AboutSection.css'
+import '../styles/AboutSection.css';
 
 const AboutSection: React.FC = () => {
     return (
-        <section className="about-section">
+        <section className="about-section my-16">
             <div className="about-column-left">
                 <h2 className="about-title text-blue-200">Über Skilt</h2>
             </div>
-            <div className="about-column-middle">
-                <p className="about-text">
-                    Skilt wurde mit dem Ziel ins Leben gerufen, das digitale Lernen im Handwerksbereich völlig neu zu gestalten. Unser Anliegen ist es, komplexes theoretisches Wissen in praxisnahe, leicht verständliche Lernmodule zu verwandeln, die genau auf die Anforderungen von Auszubildenden und Quereinsteigern zugeschnitten sind.
+            <div className="about-column-main">
+                <p className="about-text text-blue-200">
+                    Skilt ist eine Lernapp für Auszubildende im Handwerk, die komplexes Wissen in einfache und verständliche Lerneinheiten aufbricht. Durch interaktive Quizzes und kurze Wissenshäppchen wird das Lernen nicht nur effektiver, sondern auch persönlicher gestaltet. So können Nutzer jederzeit und überall auf die Inhalte zugreifen und in ihrem eigenen Tempo lernen.
                 </p>
-                <p className="about-text">
-                    Unsere Plattform macht das Lernen nicht nur einfacher, sondern fügt es nahtlos in den digitalen Alltag ein. Die Lerninhalte sind direkt und jederzeit auch ohne Internetverbindung verfügbar. Durch wiederkehrende Quizze können unsere Nutzer ihr Wissen effektiv vertiefen.
-                </p>
-                <p className="about-text">
-                    Der Schutz persönlicher Daten liegt uns am Herzen; deshalb erheben wir nur die notwendigsten Informationen, um eine sichere Lernumgebung zu gewährleisten.
-                </p>
-                <p className="about-text">
-                    Unsere Erfahrungen als Ausbilder und unsere Überzeugung von der Macht digitaler Bildung treiben die Entwicklung von Skilt voran. Skilt eignet sich hervorragend für Einzelpersonen und kleinere Unternehmen, die Wert auf qualitativ hochwertige Bildungsmaterialien legen. Unsere App befindet sich noch in der Entwicklungsphase und wir bald in den Appstores verfügbar sein.
-                </p>
-            </div>
-            <div className="about-column-right">
-                {/* This can be left empty if it's just for the background color */}
             </div>
         </section>
     );
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,47 +2,43 @@ import React from 'react';
 
 const Features: React.FC = () => {
     return (
-        <div className="py-1">
-            <div className="max-w-8xl mx-auto px-5">
+        <div className="py-2 mb-16">
+            <div className="max-w-8xl mx-auto pl-10 px-10">
                 {/* Row 1 */}
-                <div className="flex flex-wrap justify-between items-center mb-8">
+                <div className="flex flex-wrap justify-between items-center mb-12">
                     {/* Feature 1 */}
-                    <div className="flex flex-1 items-center px-1">
-                        <img src="../images/bullet1.png" alt="Expert-led video" className="mr-1" /> {/* Reduced margin even further */}
-                        <div>
+                    <div className="flex flex-1 items-center pl-10 px-1 m-6">
+                        <img src="../images/bullet1.png" alt="Expert-led video" className="ml-8" /> {/* Reduced margin even further */}
+                        <div className='mr-8'>
                             <h3 className="text-xl font-semibold mb-2">Expert-led video courses</h3>
                             <p>Find all the instructions you need to get an entry-level job in tech.</p>
                         </div>
                     </div>
-                    {/* Vertical Divider */}
-                    <div className="w-px bg-black opacity-50 mx-4 hidden lg:block"></div>
                     {/* Feature 2 */}
-                    <div className="flex flex-1 items-center px-1">
-                        <img src="../images/bullet2.png" alt="In-demand topics" className="mr-1" /> {/* Reduced margin even further */}
-                        <div>
+                    <div className="flex flex-1 items-center pl-10 px-1 m-6">
+                        <img src="../images/bullet2.png" alt="In-demand topics" className="ml-2" /> {/* Reduced margin even further */}
+                        <div className='mr-8'>
                             <h3 className="text-xl font-semibold mb-2">In-demand topics</h3>
                             <p>Stay up to date with your skills.</p>
                         </div>
                     </div>
                 </div>
-                {/* Divider Line */}
-                <div className="w-full bg-white opacity-50 h-px my-8"></div>
+                    {/* Horizontal Divider */}
+                    <div className="w-full bg-black opacity-20 h-px my-8"></div>
                 {/* Row 2 */}
                 <div className="flex flex-wrap justify-between items-center">
                     {/* Feature 3 */}
-                    <div className="flex flex-1 items-center px-1">
-                        <img src="../images/bullet3.png" alt="Segment learning" className="mr-1" /> {/* Reduced margin even further */}
-                        <div>
+                    <div className="flex flex-1 items-center pl-10 px-1 m-6">
+                        <img src="../images/bullet3.png" alt="Segment learning" className="ml-8" /> {/* Reduced margin even further */}
+                        <div className='mr-8'>
                             <h3 className="text-xl font-semibold mb-2">Segment your learning with Tracks</h3>
                             <p>Each of our Tracks is a mini-program designed to teach you a particular set of skills.</p>
                         </div>
                     </div>
-                    {/* Vertical Divider */}
-                    <div className="w-px bg-black opacity-50 mx-4 hidden lg:block"></div>
                     {/* Feature 4 */}
-                    <div className="flex flex-1 items-center px-1">
-                        <img src="../images/bullet4.png" alt="Interactive learning" className="mr-1" /> {/* Reduced margin even further */}
-                        <div>
+                    <div className="flex flex-1 items-center pl-10 px-1 my-6">
+                        <img src="../images/bullet4.png" alt="Interactive learning" className="ml-2" /> {/* Reduced margin even further */}
+                        <div className='mr-8'>
                             <h3 className="text-xl font-semibold mb-2">Interactive learning</h3>
                             <p>Our courses have quizzes and code challenges to keep you engaged.</p>
                         </div>
